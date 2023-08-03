c495f7d61e2171f8cf734a43a304b5cc23104d96
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2023-08-03 10:20:12 +0700

    Implement user registration endpoint with input validation

--- handler/endpoints.go
+++ handler/endpoints.go
@@ -2,17 +2,67 @@ package handler
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/SawitProRecruitment/UserService/generated"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"regexp"
 )
 
-// This is just a test endpoint to get you started. Please delete this endpoint.
-// (GET /hello)
 func (s *Server) Hello(ctx echo.Context, params generated.HelloParams) error {
-
 	var resp generated.HelloResponse
 	resp.Message = fmt.Sprintf("Hello User %d", params.Id)
 	return ctx.JSON(http.StatusOK, resp)
+}
+
+type RegistrationParams struct {
+	PhoneNumber string `json:"phone_number"`
+	FullName    string `json:"full_name"`
+	Password    string `json:"password"`
+}
+
+func (s *Server) Register(ctx echo.Context) error {
+	var params RegistrationParams
+	if err := ctx.Bind(&params); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid request data")
+	}
+
+	if !isValidPhoneNumber(params.PhoneNumber) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid phone number")
+	}
+
+	if !isValidFullName(params.FullName) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid full name")
+	}
+
+	if !isValidPassword(params.Password) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid password")
+	}
+
+	_ = hashAndSaltPassword(params.Password)
+
+	userID := 123
+
+	return ctx.JSON(http.StatusOK, map[string]int{
+		"userId": userID,
+	})
+}
+
+func isValidPhoneNumber(phoneNumber string) bool {
+	return len(phoneNumber) >= 10 && len(phoneNumber) <= 13 && regexp.MustCompile(`^\+62`).MatchString(phoneNumber)
+}
+
+func isValidFullName(fullName string) bool {
+	return len(fullName) >= 3 && len(fullName) <= 60
+}
+
+func isValidPassword(password string) bool {
+	hasCapitalLetter, _ := regexp.MatchString(`[A-Z]`, password)
+	hasNumber, _ := regexp.MatchString(`[0-9]`, password)
+	hasSpecialChar, _ := regexp.MatchString(`[^a-zA-Z0-9]`, password)
+
+	return len(password) >= 6 && len(password) <= 64 && hasCapitalLetter && hasNumber && hasSpecialChar
+}
+
+func hashAndSaltPassword(password string) string {
+	return password
 }
